c1f15a897894aed0620d9b2fab5c91f261eee7c8
alain lanthier <alanthier001@hotmail.ca>
2024-12-08 04:07:04 -0500

    init

---
+++ third_party/nanobench/src/docs/generate.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+# Run from src/docs directory
+#
+# Install breathe (assuming there's a venv in ~/git/venv)
+#   source ~/git/venv/bin/activate
+#   pip install -U sphinx
+#   pip install -U breathe
+#   pip install -U sphinx_rtd_theme
+#   pip install -U recommonmark
+
+# rm should work, this should copy all files, but for safety reasons I'm not doing it...
+#rm -Rvf ../../docs
+doxygen
+source ~/git/venv/bin/activate
+python ~/git/venv/bin/sphinx-build -E . ../../docs
